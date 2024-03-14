c25d677c0e327ae90075186f4b641a74d45e92fe
dominooooooo <late033@gmail.com>
2024-03-14 23:14:33 +0200

    d

--- fairplay/middleware.ts
+++ fairplay/middleware.ts
@@ -1,6 +1,23 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth(
+    function middleware() {},
+    {
+        callbacks: {
+            authorized: ({ req, token }) => {
+                // Check if the user has a token and if the pathname includes '/admin'
+                if (Boolean(token) && req.nextUrl.pathname.includes('/admin')) {
+                    // Return true only if the user's role is 'ylläpitäjä' (administrator)
+                    return token.role === 'ylläpitäjä';
+                }
+                // For all other routes, return true if there's a token
+                return Boolean(token);
+            }
+        }
+    }
+)
 
 export const config = { 
-    matcher: ["/", "/profiili/:path*", "/admin:path*"],
+    matcher: ["/", "/profiili/:path*", "/admin/:path*"],
 }
 
