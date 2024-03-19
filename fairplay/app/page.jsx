3132a42ba387c37ac5ea6a36acfdb40e9f3ecbff
jimi <lampparijimi@gmail.com>
2024-03-19 10:49:44 +0200

    logot ja muut

--- fairplay/app/page.jsx
+++ fairplay/app/page.jsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/auth";
 import { Button } from "@nextui-org/react";
 import { MdAdminPanelSettings } from "react-icons/md";
+import Image from "next/image";
 
 const Home = async () => {
   const session = await getServerSession(authOptions);
@@ -21,20 +22,20 @@ const Home = async () => {
 
   return (
     <>
-    <div className="flex justify-around bg-zinc-700 p-4 rounded-lg">
-      <div><Navbar /></div>
-      <div className="flex text-center text-xl">{greeting}, {session?.user.firstname} {session?.user.lastname}</div>
-      {isAdmin && <div><Button variant="bordered" size="lg"><MdAdminPanelSettings size={25}/>Admin</Button></div>}
-    </div>
-    <div style={{
-        backgroundImage: `url('https://bwhip62.files.wordpress.com/2023/10/dji_0759-1.jpg')`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        opacity: '40%',
-        height: '100vh'
-      }}>
-
-    </div>
+      <div className="flex justify-around bg-zinc-700 p-4 rounded-lg">
+        <div><Navbar /></div>
+        <div className="flex text-center text-xl">{greeting}, {session?.user.firstname} {session?.user.lastname}</div>
+        {isAdmin && <div><Button variant="bordered" size="lg"><MdAdminPanelSettings size={25}/>Admin</Button></div>}
+      </div>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="absolute top-4 left-4">
+          <Image src="/kuva.jpg" alt="Kuva" width={50} height={50} />
+        </div>
+        <div className="flex flex-col items-center" >
+          <h1 className="text-4xl font-bold">Fairplay</h1>
+          <p className="text-lg">2024</p>
+        </div>
+      </div>
     </>
   );
 }
