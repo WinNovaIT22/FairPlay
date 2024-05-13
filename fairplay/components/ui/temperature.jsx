3c3a8f3f83d90e94f67f15a85035f1dceb46b617
lauriseta <laurinvaratili@gmail.com>
2024-05-13 14:54:03 +0300

    g

--- fairplay/components/ui/temperature.jsx
+++ fairplay/components/ui/temperature.jsx
@@ -17,23 +17,6 @@ const Temperature = () => {
         setCity(data.city);
         setLoading(false);
 
-        const weatherCondition = data.weatherCondition.toLowerCase();
-        let icon;
-        switch (weatherCondition) {
-          case 'sunny':
-            icon = 'sun.png';
-            break;
-          case 'cloudy':
-            icon = 'cloud.png';
-            break;
-          case 'rainy':
-            icon = 'rain.png';
-            break;
-          default:
-            icon = 'default.png';
-            break;
-        }
-        setWeatherIcon(icon);
       } catch (error) {
         console.error("Error fetching temperature:", error);
         setLoading(false);
@@ -50,7 +33,6 @@ const Temperature = () => {
           <div>Ladataan säätä...</div>
         ) : (
           <div>
-            <img src={`/images/${weatherIcon}`} alt="Weather icon" />
             <span>{temperature}, {city}</span>
           </div>
         )}
